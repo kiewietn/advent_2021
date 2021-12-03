d2d66be8fe743de815a2050246ea9140bc01be77
Nico Kiewiet <ngkiewiet@gmail.com>
2021-12-03 17:25:53 +0200

    Day3

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -45,3 +45,46 @@
   (let [final-coord (reduce (fn [acc move]
               (part2-get-new-pos acc move)) (list 0 0 0) moves)]
     (* (first final-coord) (first (rest final-coord)))))
+
+(defn get-freq [input]
+  (into [] (map #(frequencies %) (apply mapv vector input))))
+
+(defn parse-to-decimal [binary-seq]
+  (Integer/parseInt (apply str binary-seq) 2))
+
+(defn invert-binary [binary]
+  (map #(if (= \0 %) \1 \0) binary))
+
+(defn gamma-binary [input]
+  (map (fn [freq]
+         (let [ones (get freq \1)
+               zeros (get freq \0)]
+           (if (> ones zeros) \1  \0))) (get-freq input)))
+
+(defn day3_1 [input]
+  (let [gamma-bin (gamma-binary input)
+        epsilon-bin (invert-binary gamma-bin)]
+    (* (parse-to-decimal gamma-bin) (parse-to-decimal epsilon-bin))))
+
+(defn oxy-criteria [index input]
+  (let [freq (get (get-freq input) index)
+        ones (get freq \1)
+        zeros (get freq \0)]
+    (if (>= ones zeros) \1 \0)))
+
+(defn co2-criteria [index input]
+  (first (invert-binary (list (oxy-criteria index input)))))
+
+(defn filter-on-criteria [criteria index input]
+  (filter #(= criteria (get % index)) input))
+
+(defn find-value [criteria-filter-fn input]
+  (loop [index 0
+         result (filter-on-criteria (criteria-filter-fn index input) index input)]
+    (if (= 1 (count result)) result
+        (recur (inc index) (filter-on-criteria (criteria-filter-fn (inc index) result) (inc index) result)))))
+
+(defn day3_2 [input]
+  (let [oxygen-rating (parse-to-decimal (first (find-value oxy-criteria input)))
+        co2-rating (parse-to-decimal (first (find-value co2-criteria input)))]
+    (* oxygen-rating co2-rating)))
